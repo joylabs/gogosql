5f16d3a705bb4577d4d2dc838d7bd5ae98214e68
John McConnell <john.mcconnell@memohq.com>
2022-05-09 08:27:36 -0700

    adding e1 example

--- examples/e1.out.go
+++ examples/e1.out.go
@@ -1,71 +1,130 @@
-package gogosql
+package kipsql
 
 import (
-	"context"
-	"fmt"
-	"time"
-
-	"cloud.google.com/go/spanner"
+  "time"
+  "cloud.google.com/go/spanner"
 )
 
 type SpannerTX interface {
 	ReadRow(context.Context, string, spanner.Key, []string) (*spanner.Row, error)
 }
 
-// TableDefinitions
-type GoGoMessagesColumns struct {
-	UserId    string
-	MessageId string
-	Content   string
-	CreatedAt string
-	All       []string
+// Column Definitions
+type GoGoMessagesColumn struct {
+    UserId string
+    MessageId string
+    Content string
+    CreatedAt string
+    UpdatedAt string
+    IsDeleted string
+    All []string
+}
+type GoGoCommunitiesColumn struct {
+    CommunityId string
+    VoteCount string
+    Description string
+    All []string
 }
 
+// Table Definitions
 type GoGoMessagesTable struct {
-	Columns   GoGoMessagesColumns
-	TableName string
+    Columns GoGoMessagesColumn
+    TableName string
+}
+type GoGoCommunitiesTable struct {
+    Columns GoGoCommunitiesColumn
+    TableName string
 }
 
+// All Table Definitions
 type GoGoTables struct {
-	Messages GoGoMessagesTable
+    Messages GoGoMessagesTable
+    Communities GoGoCommunitiesTable
 }
 
-// RecordDefinitions
+// PrimaryKey Definitions
+type GoGoMessagesPrimaryKey struct {
+    UserId string
+    MessageId string
+}
+type GoGoCommunitiesPrimaryKey struct {
+    CommunityId string
+}
+
+// Record Definitions
 type Message struct {
-	UserId    string    `spanner:"user_id"`
-	MessageId string    `spanner:"message_id"`
-	Content   string    `spanner:"content"`
-	CreatedAt time.Time `spanner:"created_at"`
+    UserId string `spanner:"user_id"`
+    MessageId string `spanner:"message_id"`
+    Content string `spanner:"content"`
+    created_at time.Time `spanner:"created_at"`
+    UpdatedAt time.Time `spanner:"updated_at"`
+    IsDeleted bool `spanner:"is_deleted"`
 }
-
-type MessageOpt struct {
-	UserId    *string
-	MessageId *string
-	Content   *string
-	CreatedAt *time.Time
+type PartialMessage struct {
+    UserId *string
+    MessageId *string
+    Content *string
+    created_at *time.Time
+    UpdatedAt *time.Time
+    IsDeleted *bool
+}
+type Comm struct {
+    CommunityId string `spanner:"community_id"`
+    VoteCount int `spanner:"vote_count"`
+    Description string `spanner:"description"`
+}
+type PartialComm struct {
+    CommunityId *string
+    VoteCount *int
+    Description *string
 }
 
 var (
-	KipTables = GoGoTables{
-		Messages: GoGoMessagesTable{
-			Columns: GoGoMessagesColumns{
-				UserId:    "user_id",
-				MessageId: "message_id",
-				Content:   "content",
-				CreatedAt: "created_at",
-				All: []string{
-					"user_id",
-					"message_id",
-					"content",
-					"created_at",
-				},
-			},
-		},
-	}
+    Tables = GoGoTables{
+        Messages: GoGoMessagesTable{
+            Columns: GoGoMessagesColumn{
+                UserId: "user_id",
+                MessageId: "message_id",
+                Content: "content",
+                CreatedAt: "created_at",
+                UpdatedAt: "updated_at",
+                IsDeleted: "is_deleted",
+                All: []string{
+                    "user_id",
+                    "message_id",
+                    "content",
+                    "created_at",
+                    "updated_at",
+                    "is_deleted",
+                },
+            },
+            TableName: "messages",
+        },
+        Communities: GoGoCommunitiesTable{
+            Columns: GoGoCommunitiesColumn{
+                CommunityId: "community_id",
+                VoteCount: "vote_count",
+                Description: "description",
+                All: []string{
+                    "community_id",
+                    "vote_count",
+                    "description",
+                },
+            },
+            TableName: "communities",
+        },
+  }
 )
 
-func (GoGoMessagesTable) NewSpannerKey(userId string, messageId string) spanner.Key {
-	return spanner.Key{userId, messageId}
+// Methods
+func (GoGoMessagesTable) PrimaryKey (
+    userId,
+    messageId,
+) GoGoMessagesPrimaryKey {
+	return GoGoMessagesPrimaryKey{
+            userId,
+            messageId,
+        }
 }
 
 func (t GoGoMessagesTable) ReadRow(ctx context.Context, tx SpannerTX, key spanner.Key) (*Message, error) {
@@ -79,18 +138,71 @@ func (t GoGoMessagesTable) ReadRow(ctx context.Context, tx SpannerTX, key spanne
 	return r, err
 }
 
-func (r Message) SpannerKey() spanner.Key {
-	return spanner.Key{r.UserId, r.MessageId}
+func (r Message) PrimaryKey() GoGoMessagesPrimaryKey {
+	return GoGoMessagesPrimaryKey{
+            r.UserId,
+            r.MessageId,
+        }
 }
 
-func (r MessageOpt) SpannerKey() (spanner.Key, error) {
-	if r.UserId == nil {
-		return nil, fmt.Errorf("UserId is required to make a spanner key for: %v", r)
+func (r PartialMessage) PrimaryKey() (GoGoMessagesPrimaryKey, error) {
+    if r.UserId == nil {
+        return GoGoMessagesPrimaryKey{}, fmt.Errorf("UserId is required to make a spanner key for: %v", r)
+    }
+    if r.MessageId == nil {
+        return GoGoMessagesPrimaryKey{}, fmt.Errorf("MessageId is required to make a spanner key for: %v", r)
+    }
+
+    return GoGoMessagesPrimaryKey{
+            *r.UserId,
+            *r.MessageId,
+        }, nil
+    }
+
+func (r GoGoMessagesPrimaryKey) SpannerKey() spanner.Key {
+	return spanner.Key{
+            r.UserId,
+            r.MessageId,
+        }
+}
+
+func (GoGoCommunitiesTable) PrimaryKey (
+    communityId,
+) GoGoCommunitiesPrimaryKey {
+	return GoGoCommunitiesPrimaryKey{
+            communityId,
+        }
+}
+
+func (t GoGoCommunitiesTable) ReadRow(ctx context.Context, tx SpannerTX, key spanner.Key) (*Comm, error) {
+	row, err := tx.ReadRow(ctx, t.TableName, key, t.Columns.All)
+	if err != nil {
+		return nil, err
 	}
 
-	if r.MessageId == nil {
-		return nil, fmt.Errorf("MessageId is required to make a spanner key for: %v", r)
-	}
+	r := &Comm{}
+	err = row.ToStruct(r)
+	return r, err
+}
 
-	return spanner.Key{*r.UserId, *r.MessageId}, nil
+func (r Comm) PrimaryKey() GoGoCommunitiesPrimaryKey {
+	return GoGoCommunitiesPrimaryKey{
+            r.CommunityId,
+        }
+}
+
+func (r PartialComm) PrimaryKey() (GoGoCommunitiesPrimaryKey, error) {
+    if r.CommunityId == nil {
+        return GoGoCommunitiesPrimaryKey{}, fmt.Errorf("CommunityId is required to make a spanner key for: %v", r)
+    }
+
+    return GoGoCommunitiesPrimaryKey{
+            *r.CommunityId,
+        }, nil
+    }
+
+func (r GoGoCommunitiesPrimaryKey) SpannerKey() spanner.Key {
+	return spanner.Key{
+            r.CommunityId,
+        }
 }
